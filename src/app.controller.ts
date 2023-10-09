dd67c4cffb3f402b9a3ad255c244a84bb9e2d102
s1n <ohdens@gmail.com>
2023-10-10 02:02:26 +0900

    enable CORS mode.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { request } from 'http';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Req() req: Request): string {
+    console.log(`${new Date().toLocaleString('sv')} ${req.method}[${req.url}]`);
     return this.appService.getHello();
+  }
+
+  @Post()
+  getPost(@Req() req: Request): any {
+    console.log(`${new Date().toLocaleString('sv')} ${req.method}[${req.url}]`);
+    return { message: 'hello restjs world.' };
   }
 }
