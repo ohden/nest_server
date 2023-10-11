55cb84e17d7f67433eaac812ab00b3b321b5a90d
s1n <ohdens@gmail.com>
2023-10-11 21:36:22 +0900

    Response:413対策にlimit size拡大

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors();
+
+  app.useBodyParser('json', { limit: '10mb' }); //application/json利用時のlimit size.
+  app.useBodyParser('urlencoded', { limit: '100mb', extended: true }); //URL encode形式のpost dataのlimit size.
 
   await app.listen(3000);
 }
